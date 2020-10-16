e7b159068b9949a8fa20e290d55d69024ab84888
dev-swd <tladnjsen@gmail.com>
2020-10-17 02:13:49 +0900

    update 010

--- 010_duplicate_numbers .py
+++ 010_duplicate_numbers .py
@@ -5,12 +5,53 @@
 sample inputs:  0123456789 01234 01234567890 6789012345 012322456789
 sample outputs: true       false false       true       false
 """
-
 # 입력받은 숫자의 갯수가 0~9 까지 10개이어야 한다.
 # 중복 값이 있다면 false, 없다면 true
-num_list = list(input().split(" "))
-for n in num_list:
-    if len(n) != 10:
-        print(False)
-    else:
-        print(False) if len(set(n)) != 10 else print(True)
+
+
+def judge_duplicate_num(func):
+    num_list = func.split(" ")
+
+    # 1
+    result_list = list(map(lambda i: len(i) == len(set(i)) == 10, num_list))
+    print(result_list)
+
+    # 2
+    for i in num_list:
+        print("true" if len(i) == len(set(i)) == 10 else "false", end=" ")
+
+    print()
+
+    # 3
+    for i in num_list:
+        if len(i) != 10:
+            print(False, end=" ")
+        else:
+            print(False, end=" ") if len(set(i)) != 10 else print(True, end=" ")
+
+
+judge_duplicate_num(input())
+
+# 첫번 째
+# num_list = list(input().split(" "))
+# for n in num_list:
+#     if len(n) != 10:
+#         print(False)
+#     else:
+#         print(False) if len(set(n)) != 10 else print(True)
+
+# 두번 째
+# for i in input().split(" "):
+#     if len(i) != 10:
+#         print(False)
+#     else:
+#         print(False) if len(set(i)) != 10 else print(True)
+
+#세번 째
+# for i in input().split(" "):
+#     print("true" if len(i) == len(set(i)) == 10 else "false")
+
+
+# 네번 째
+# result_list = list(map(lambda i: len(i) == len(set(i)) == 10, input().split()))
+# print(result_list)
