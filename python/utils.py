8304bccb356782a491b0740491986ebe6191566e
dev-swd <tladnjsen@gmai.com>
2021-01-19 12:56:26 +0900

    [2021-01-19: SWD] add pakage python-algorithm-interview in python pakage -add init.py -add utils.py (timer decorator) -quiz_01.py in python-algorithm-interview pakage

---
+++ python/utils.py
@@ -0,0 +1,16 @@
+import time
+
+
+class CommonUtils:
+    
+    @staticmethod
+    def logging_time(original_fn):
+        def wrapper_fn(*args, **kwargs):
+            start_time = time.time()
+            result     = original_fn(*args, **kwargs)
+            end_time   = time.time()
+            
+            print('Working Time[{}]: {} sec'. format(original_fn.__name__, end_time-start_time))
+            return result
+        
+        return wrapper_fn
