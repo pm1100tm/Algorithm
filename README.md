7085a9136b43d95bf99379483e3b313c8e4c7a33
dev-swd <tladnjsen@gmai.com>
2021-01-30 12:43:23 +0900

    [2021-01-30:SWD] add lv_one_001.py, lv_one_002.py to programmers

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 - ***[코딩도장](https://codingdojang.com/)***
 - ***[리트코드](https://leetcode.com/)***
 - ***[프로그래머스](https://programmers.co.kr/)***
-- **파이썬 알고리즘 인터뷰 도서**
+- ***파이썬 알고리즘 인터뷰 도서***
--- python/programmers/lv_one_001.py
+++ python/programmers/lv_one_001.py
@@ -1,2 +1,78 @@
-""" 크레인 인형뽑기 게임
+""" 두 정수 사이의 합
+
+두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+
+제한 조건
+    a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
+    a와 b는 -10,000,000 이상 10,000,000 이하인 정수입니다.
+    a와 b의 대소관계는 정해져있지 않습니다.
+
+입출력 예
+    a   b   return
+    3   5   12
+    3   3   3
+    5   3   12
 """
+import unittest
+
+def solution_1(a, b):
+    
+    answer = 0
+    
+    if a > b:
+        a, b = b, a
+    
+    for num in range(a, b+1):
+        answer += num
+        
+    return answer
+
+def solution_2(a, b):
+    
+    if a == b:
+        return a
+    
+    if a > b:
+        a, b = b, a
+        
+    return sum(range(a, b + 1))
+
+
+class TestSolution(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.case_1 = (3, 5)
+        self.case_2 = (3, 3)
+        self.case_3 = (5, 3)
+    
+    def test_1_solution_1(self):
+        self.assertEqual(
+            12,
+            solution_1(
+                self.case_1[0],
+                self.case_1[1]
+            )
+        )
+    
+    def test_2_solution_1(self):
+        self.assertEqual(
+            3,
+            solution_1(
+                self.case_2[0],
+                self.case_2[1]
+            )
+        )
+    
+    def test_3_solution_1(self):
+        self.assertEqual(
+            12,
+            solution_1(
+                self.case_3[0],
+                self.case_3[1]
+            )
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
