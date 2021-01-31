e4f383f12b23c9f5c49bc802bb0fb0f8a29bf053
dev-swd <tladnjsen@gmai.com>
2021-02-01 01:16:30 +0900

    [2021-02-01:SWD] add lv_one_006.py to programmers

---
+++ python/programmers/lv_one_006.py
@@ -0,0 +1,113 @@
+import unittest
+import traceback
+from python.utils import CommonUtils
+
+
+class Solution:
+    """ 문자열 다루기 기본
+        문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요.
+        예를 들어 s가 a234이면 False를 리턴하고 1234라면 True를 리턴하면 됩니다.
+        
+        제한 사항
+            s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+        
+        입출력 예
+            a234    false
+            1234    true
+        
+        위의 문제에 대한 솔루션 메서드를 1개 이상 작성하기
+    """
+    
+    @CommonUtils.logging_time
+    def my_solution(self, s: str) -> bool:
+        if len(s) == 4 or len(s) == 6:
+            
+            if s.isdigit():
+                return True
+        
+        return False
+    
+    @CommonUtils.logging_time
+    def solution_one(self, s: str) -> bool:
+        return s.isdigit() and len(s) in (4, 6)
+
+    @CommonUtils.logging_time
+    def solution_two(self, s: str) -> bool:
+        import re
+        return bool(re.match("^(\d{4}|\d{6})$", s))
+
+    @CommonUtils.logging_time
+    def solution_three(self, s: str) -> bool:
+        try:
+            int(s)
+        except ValueError as e:
+            traceback.print_exc(e)
+            return False
+        
+        except TypeError as e:
+            traceback.print_exc(e)
+            return False
+        
+        return len(s) == 4 or len(s) == 6
+
+
+class TestSolution(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.solution = Solution()
+        self.case_001 = '1234'
+        self.case_002 = 'a123'
+        self.case_003 = '123456'
+        self.case_004 = '12b3a4'
+    
+    def test_my_solution_case_001(self):
+        case_001_result = self.solution.my_solution(self.case_001)
+        
+        self.assertEqual(
+            True,
+            case_001_result
+        )
+    
+    def test_my_solution_case_002(self):
+        case_002_result = self.solution.my_solution(self.case_002)
+        
+        self.assertEqual(
+            False,
+            case_002_result
+        )
+    
+    def test_my_solution_case_003(self):
+        case_003_result = self.solution.my_solution(self.case_003)
+        
+        self.assertEqual(
+            True,
+            case_003_result
+        )
+
+    def test_my_solution_case_004(self):
+        case_004_result = self.solution.my_solution(self.case_004)
+        
+        self.assertEqual(
+            False,
+            case_004_result
+        )
+    
+    def test_solution_one(self):
+        case_result_list = [
+            self.solution.solution_one(self.case_001),
+            self.solution.solution_one(self.case_002),
+            self.solution.solution_one(self.case_003),
+            self.solution.solution_one(self.case_004)
+        ]
+        
+        expect_list = [True, False, True, False]
+        
+        for index, result in enumerate(case_result_list):
+            self.assertEqual(
+                expect_list[index],
+                result
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
