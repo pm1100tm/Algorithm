638942b0c241d6a614973c4e91730d77f6084e3f
dev-swd <tladnjsen@gmail.com>
2021-10-17 01:07:53 +0900

    <feature:quiz_007> 2021-10-17 SWD - make dir test_algorithm_interview - add test case

--- tests/test_algorithm_interview/test_quiz_007.py
+++ tests/test_algorithm_interview/test_quiz_007.py
@@ -14,3 +14,17 @@ def test_solution_002(test_date_002):
     )
 
     assert result == test_date_002['expected']
+
+def test_solution_003(test_date_003):
+    result = TargetIndex.my_solution_one(
+        test_date_003['nums'], test_date_003['target']
+    )
+
+    assert result == test_date_003['expected']
+
+def test_solution_004(test_date_004):
+    result = TargetIndex.my_solution_one(
+        test_date_004['nums'], test_date_004['target']
+    )
+
+    assert result == test_date_004['expected']
